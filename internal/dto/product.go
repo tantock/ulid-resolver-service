6af81db9768b32dd7d43904202f9a9208a167480
tantock <tangabrielkl@gmail.com>
2026-07-15 17:32:49 -0400

    Required Type for query parameter

--- internal/dto/product.go
+++ internal/dto/product.go
@@ -2,5 +2,5 @@ package dto
 
 type ProductIdInput struct {
 	Id   string `path:"id" example:"8801052435022" doc:"Product Identifier"`
-	Type string `query:"type"`
+	Type string `query:"type" required:"true"`
 }
