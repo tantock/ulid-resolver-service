02c66ad2525d8effbffe8d36051b549feedb289e
tantock <tangabrielkl@gmail.com>
2026-07-16 14:06:11 -0400

    Added query file

---
+++ queries/query.sql
@@ -0,0 +1,7 @@
+-- name: SelectUlidFromId :one
+SELECT *
+FROM product_code_type pct
+JOIN product p
+    ON pct.id = p.product_code_type_id
+WHERE pct.display_name = $1 
+    AND p.product_code = $2;
