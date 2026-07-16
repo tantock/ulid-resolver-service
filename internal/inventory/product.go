81ed7554502ed3ec63fc5bb3c11ab9a84d4e7925
tantock <tangabrielkl@gmail.com>
2026-07-16 16:12:23 -0400

    Added Product interface and UPC ctor

--- internal/inventory/product.go
+++ internal/inventory/product.go
@@ -2,9 +2,31 @@ package inventory
 
 import (
 	"github.com/oklog/ulid/v2"
+	"github.com/tantock/ulid-resolver-service/internal/dto"
 )
 
-type Product struct {
-	ID   string
-	ULID ulid.ULID
+type Product interface {
+	Id() string
+	IdType() dto.IdType
+}
+
+type product struct {
+	id     string
+	idType dto.IdType
+	ulid   ulid.ULID
+}
+
+// Id implements [Product].
+func (p *product) Id() string {
+	return p.id
+}
+
+// IdType implements [Product].
+func (p *product) IdType() dto.IdType {
+	panic("unimplemented")
+}
+
+func NewUpcProduct(UPC string, ULID ulid.ULID) Product {
+	product := product{id: UPC, ulid: ULID, idType: dto.IdUpc}
+	return &product
 }
