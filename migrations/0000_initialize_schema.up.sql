3a9d99b13ac9e7129905e932eff1e28385ec3e39
tantock <tangabrielkl@gmail.com>
2026-07-16 12:40:50 -0400

    Added db migration file

---
+++ migrations/0000_initialize_schema.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE product_code_type(
+    id int PRIMARY KEY,
+    display_name TEXT NOT NULL,
+    CONSTRAINT product_code_type_display_name_unique
+        UNIQUE (display_name)
+);
+
+CREATE TABLE product (
+    id TEXT PRIMARY KEY,
+    product_code TEXT NOT NULL,
+    product_code_type_id int NOT NULL REFERENCES product_code_type(id),
+    CONSTRAINT product_id_ulid_check
+        CHECK (id ~ '^[0-9A-HJKMNP-TV-Z]{26}$'),
+    CONSTRAINT product_product_code_unique
+        UNIQUE (product_code)
+);
