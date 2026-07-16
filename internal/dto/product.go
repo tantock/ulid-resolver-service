a9547e61b8f7b2e32bc77fd9f4e83c576a22c6ad
tantock <tangabrielkl@gmail.com>
2026-07-15 20:12:57 -0400

    Changed type to const enum

--- internal/dto/product.go
+++ internal/dto/product.go
@@ -1,6 +1,12 @@
 package dto
 
+type IdType string
+
+const (
+	IdUpc IdType = "upc"
+)
+
 type ProductIdInput struct {
 	Id   string `path:"id" example:"8801052435022" doc:"Product Identifier"`
-	Type string `query:"type" required:"true"`
+	Type IdType `query:"type" required:"true"`
 }
