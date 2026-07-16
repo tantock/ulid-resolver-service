7dac2607795e15ae0c67b78bf6082eafaf7244a5
tantock <tangabrielkl@gmail.com>
2026-07-16 09:23:44 -0400

    Added TransformSchema to enum for doc gen

--- internal/dto/product.go
+++ internal/dto/product.go
@@ -1,10 +1,19 @@
 package dto
 
+import "github.com/danielgtaylor/huma/v2"
+
 type IdType string
 
 const (
 	IdUpc IdType = "upc"
 )
+
+func (IdType) TransformSchema(r huma.Registry, s *huma.Schema) *huma.Schema {
+	s.Enum = []any{
+		string(IdUpc),
+	}
+	return s
+}
 
 type ProductIdInput struct {
 	Id   string `path:"id" example:"8801052435022" doc:"Product Identifier"`
